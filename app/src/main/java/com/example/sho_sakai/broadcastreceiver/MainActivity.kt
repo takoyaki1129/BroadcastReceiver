ca85bfafd82401c06de9e00ecc223e5974bfdeff
Sho_SAKAI <sakaisho_1984@yahoo.co.jp>
2019-12-09 22:00:25 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sho_sakai/broadcastreceiver/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sho_sakai.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.widget.Toast
+import android.content.Intent
+
+class TimezoneBroadcastReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Toast.makeText(context, "タイムゾーンが変化しました", Toast.LENGTH_LONG).show()
+    }
+}
